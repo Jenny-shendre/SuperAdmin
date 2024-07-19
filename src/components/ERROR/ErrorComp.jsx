a7bb787d9059320d5cd93d4f9a69cf1753f08259
Jennifer Shendre <105356506+Jenny-shendre@users.noreply.github.com>
2024-07-19 11:43:42 +0530

    Delete src directory

--- src/components/ERROR/ErrorComp.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ErrorComp = () => {
-  return <div>Error</div>;
-};
-
-export default ErrorComp;
--- src/pages/Channel_Visitors/Channel_Visitors.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Channel_Visitors = () => {
-  return <div>Channel_Visitors</div>;
-};
-
-export default Channel_Visitors;
